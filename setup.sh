7196d4269015b0b5a5516fa6cfb53615f2748093
Pedro Lemes <113155395+code-pedrolsl@users.noreply.github.com>
2026-06-07 07:29:00 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# Roda uma vez em cada máquina após clonar o repositório
-
 pip install grpcio grpcio-tools
 
-# Gera os arquivos gRPC a partir dos .proto
 python3 -m grpc_tools.protoc -I proto --python_out=. --grpc_python_out=. proto/scoreboard.proto
 python3 -m grpc_tools.protoc -I proto --python_out=. --grpc_python_out=. proto/coordinator.proto
 
-# Copia para as subpastas que precisam importar
 cp scoreboard_pb2*.py   server/
 cp scoreboard_pb2*.py   coordinator/
 cp scoreboard_pb2*.py   client/
